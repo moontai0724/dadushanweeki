9ddb0d4e27d5f00726e4d0f5e4d3f11cefe9c332
月太げつたい <me@moontai0724.tw>
2020-11-30 19:09:07 +0800

    Popup v1 finished

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 document.addEventListener("DOMContentLoaded", initialize);
 
 function initialize() {
+  registerComponents();
+
   const vueImpl = new Vue({
     el: document.getElementById("app"),
-    data: {
-      menuOpened: false,
+    data() {
+      return {
+        menuOpened: false,
+        popupOpened: true,
+        currentPlace: undefined,
+        width: 1920,
+        height: 1080,
+      };
     },
     methods: {
+      resizeHandler() {
+        let mountain = this.$refs.mountain;
+        this.width = mountain.width;
+        this.height = mountain.height;
+        console.log("Mountain size changed to ", this.width, this.height);
+      }
+    },
+    created() {
+      window.addEventListener("resize", this.resizeHandler);
+    },
+    destroyed() {
+      window.removeEventListener("resize", this.resizeHandler);
+    },
+  });
+}
+
+function registerComponents() {
+  Vue.component("popup-template", {
+    template: document.getElementById("popup"),
+    props: ["place"],
+    methods: {
+      keyDownHandler(event) {
+        if (event.keyCode === 27)
+          this.closePopup();
+      },
+      closePopup() {
+        this.$emit("close");
+      }
+    },
+    created() {
+      window.addEventListener("keydown", this.keyDownHandler);
+    },
+    destroyed() {
+      window.removeEventListener("keydown", this.keyDownHandler);
     },
   });
 }
