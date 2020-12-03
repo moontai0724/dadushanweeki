515c48762d8349115d78944c59ee33c56ad45de4
月太げつたい <me@moontai0724.tw>
2020-12-03 14:13:41 +0800

    新增多筆資料

--- data.js
+++ data.js
@@ -4,10 +4,9 @@ const PLACES = [
     "iconStyle": {
       "max-width": "20%",
       "max-height": "20%",
-      "left": "25%",
+      "left": "28%",
       "top": "50%",
     },
-    "cover": "",
     "title": "磺溪書院",
     "address": "臺中市大肚區文昌路60號",
     "descriptions": [
@@ -19,24 +18,23 @@ const PLACES = [
         "description": "磺溪書院",
         "url": "./assets/12/1.jpg",
         "source": ""
-      }
+      },
     ],
     "referances": [
       {
         "title": "維基百科",
         "url": "https://zh.wikipedia.org/wiki/%E7%A3%BA%E6%BA%AA%E6%9B%B8%E9%99%A2"
-      }
+      },
     ]
   },
   {
     "icon": "./assets/17/icon.png",
     "iconStyle": {
-      "max-width": "20%",
-      "max-height": "20%",
-      "bottom": "0%",
-      "right": "5%",
+      "max-width": "15%",
+      "max-height": "15%",
+      "bottom": "5%",
+      "right": "28%",
     },
-    "cover": "",
     "title": "大肚花海",
     "address": "台中市大肚區沙田路一段旁",
     "descriptions": [
@@ -48,13 +46,168 @@ const PLACES = [
         "description": "大肚花海",
         "url": "./assets/17/1.jpg",
         "source": "https://news.ltn.com.tw/news/life/breakingnews/3027518"
-      }
+      },
     ],
     "referances": [
       {
         "title": "維基百科",
         "url": "https://zh.wikipedia.org/wiki/%E7%A3%BA%E6%BA%AA%E6%9B%B8%E9%99%A2"
-      }
+      },
+    ]
+  },
+  {
+    "icon": "./assets/30/icon.png",
+    "iconStyle": {
+      "max-width": "15%",
+      "max-height": "15%",
+      "bottom": "20%",
+      "left": "22%",
+    },
+    "title": "大肚山威靈仙",
+    "address": "",
+    "descriptions": [
+      "台灣特有種，分布在中部區域，開花期約為 5 ~ 7 月，可用於中藥材，用途為筋通脈絡、治療破傷風等。",
+    ],
+    "images": [],
+    "referances": [
+      {
+        "title": "認識植物",
+        "url": "http://kplant.biodiv.tw/%E5%A4%A7%E8%82%9A%E5%B1%B1%E5%A8%81%E9%9D%88%E4%BB%99/%E5%A4%A7%E8%82%9A%E5%B1%B1%E5%A8%81%E9%9D%88%E4%BB%99.htm"
+      },
+    ]
+  },
+  {
+    "icon": "./assets/20/icon.png",
+    "iconStyle": {
+      "max-width": "8%",
+      "max-height": "8%",
+      "top": "40%",
+      "left": "40%",
+    },
+    "title": "九天玄女廟",
+    "address": "台中市大雅區忠義村清泉路99號",
+    "descriptions": [
+      "九天玄女廟位於大肚山上靠近台中都會公園之處，其中有專門收留中輟生所組成的九天民俗技藝團。",
+      "前方的廣場就是團員每天練鼓的地方，而一旁的貨櫃屋就是團員們居住的地方。",
+      "而這裡也是電影「陣頭」的拍攝場景！",
+      "九天玄女廟位於大肚山上靠近台中都會公園之處，其中有專門收留中輟生所組成的九天民俗技藝團。前方的廣場就是團員每天練鼓的地方，而一旁的貨櫃屋就是團員們居住的地方。而這裡也是電影「陣頭」的拍攝場景！"
+    ],
+    "images": [
+      {
+        "description": "九天玄女廟",
+        "url": "./assets/20/1.jpg",
+        "source": "https://fbmax0623.tian.yam.com/posts/48678459"
+      },
+    ],
+    "referances": [
+      {
+        "title": "四方通行旅遊",
+        "url": "https://guide.easytravel.com.tw/scenic/71091"
+      },
+    ]
+  },
+  {
+    "icon": "./assets/34/icon.png",
+    "iconStyle": {
+      "max-width": "15%",
+      "max-height": "15%",
+      "bottom": "2%",
+      "left": "20%",
+    },
+    "title": "石虎",
+    "address": "",
+    "descriptions": [
+      "分布於海拔 1000 公尺以下，純肉食性動物，主要獵捕夜間活動的鳥類、昆蟲、小型哺乳類。",
+      "近幾年因為棲息地的減少與人為的破壞，而被列為瀕臨絕種的動物。",
+      "相關的還有石虎侵擾雞舍事件。",
+    ],
+    "images": [],
+    "referances": [
+      {
+        "title": "認識石虎",
+        "url": "https://e-info.org.tw/leopardcatnews#about"
+      },
+      {
+        "title": "石虎侵擾雞舍事件",
+        "url": "https://tw.appledaily.com/supplement/20180401/7H6RERH2A5VD6BYTT6VHCMCC4M/"
+      },
+    ]
+  },
+  {
+    "icon": "./assets/25/icon.png",
+    "iconStyle": {
+      "max-width": "12%",
+      "max-height": "12%",
+      "top": "40%",
+      "right": "25%",
+    },
+    "title": "火燒山",
+    "address": "",
+    "descriptions": [
+      "大肚山主要植被為大黍草，而秋冬乾季時乾枯的大黍草，卻成為火燒的主要燃料。",
+      "火燒山事件通常為乾枯的野草遭丟棄的菸蒂、燃燒廢棄物所引起，周邊的墓地數量多，焚燒野草及燒金紙也會成為火燒山的原因之一。",
+    ],
+    "images": [],
+    "referances": [
+      {
+        "title": "大紀元",
+        "url": "https://www.epochtimes.com/b5/19/11/21/n11670322.htm"
+      },
+      {
+        "title": "CTWANT",
+        "url": "https://www.ctwant.com/article/14002"
+      },
+    ]
+  },
+  {
+    "icon": "./assets/33/icon.png",
+    "iconStyle": {
+      "max-width": "12%",
+      "max-height": "12%",
+      "bottom": "2%",
+      "left": "10%",
+    },
+    "title": "臺灣野梨",
+    "address": "",
+    "descriptions": [
+      "台灣特有種，產地分佈在中部海拔500~1800公尺的森林",
+      "又稱為山梨，為梨屬植物中台灣特產的唯一樹種",
+      "3~4月會開出白色的花朵，果實可食用",
+      "與豆梨十分相似，但台灣野梨果實較大，葉緣有細尖的鋸齒狀且分佈的海拔較高。",
+      "臺灣植物紅皮書中，被列為嚴重瀕臨絕滅的物種。",
+      "其中一株臺灣野梨已有上百年的歷史是目前已知最古老、高大的。",
+    ],
+    "images": [],
+    "referances": [
+      {
+        "title": "杉林溪自然生態教育中心",
+        "url": "https://nature.goto307.com.tw/plantae/ins.php?index_id=212"
+      },
+      {
+        "title": "一個人與花草的生活",
+        "url": "https://www.facebook.com/horng.leaf/posts/1358632510891464/"
+      },
+    ]
+  },
+  {
+    "icon": "./assets/35/icon.png",
+    "iconStyle": {
+      "max-width": "12%",
+      "max-height": "12%",
+      "bottom": "20%",
+      "right": "20%",
+    },
+    "title": "琉璃紋鳳蝶",
+    "address": "",
+    "descriptions": [
+      "主要分布於低、中海拔地區，為台灣特有種，後翅左右各有一枚藍色的大斑，喜歡到到處吸蜜，飛行極快，以山刈葉、三腳虌為主食。",
+    ],
+    "images": [],
+    "referances": [
+      {
+        "title": "杉林溪自然生態教育中心",
+        "url": "https://nature.goto307.com.tw/animalia/ins.php?index_id=126"
+      },
     ]
   },
 ];
@@ -62,7 +215,6 @@ const PLACES = [
 const MOUNTAIN = {
   "icon": "",
   "iconStyle": {},
-  "cover": "",
   "title": "大肚臺地",
   "address": "",
   "descriptions": [
